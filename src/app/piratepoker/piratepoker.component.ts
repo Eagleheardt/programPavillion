8649946e74492f7ebbecf0c654af4a19bb2a1f07
Eagleheardt <andregueret@gmail.com>
2019-02-07 22:00:26 -0600

    added place cards

--- src/app/piratepoker/piratepoker.component.ts
+++ src/app/piratepoker/piratepoker.component.ts
@@ -19,6 +19,33 @@ export class PiratepokerComponent implements OnInit {
   // GOAL: put 10 random cards on the screen
   // GOAL: put 10 random cards on the screen, from one deck
 
+  private clearNodes = (aNode: Node, amtToLeave: number = 2) => {
+    while(aNode.childNodes.length > amtToLeave){
+      aNode.removeChild(aNode.lastChild);
+    }
+  }
+
+  private placeCards = (somePlayer: Player, IDPreFix: string, cardClassName: string, IDwhereItGoes: string) => {
+    for (var i = 0; i < somePlayer.hand.length; i++) {
+
+      var img = document.createElement("img");
+      
+      img.setAttribute("id", IDPreFix + "-" + i);
+      img.style.minWidth = this.CARD_WIDTH_PIXELS;
+      img.style.width = this.CARD_WIDTH_PERCENT;
+      img.style.maxWidth = this.CARD_WIDTH_PERCENT;
+      img.style.marginRight = this.CARD_MARGIN;
+      img.className = cardClassName;
+  
+      img.setAttribute("value", i.toString());
+
+      img.src = somePlayer.hand[i].image;
+  
+      var src = document.getElementById(IDwhereItGoes);
+      src.appendChild(img);
+    }
+  }
+
   constructor() { }
 
   ngOnInit() {
