57f7be21a221e0d640932347122e42665ed750b2
Eagleheardt <andregueret@gmail.com>
2019-02-07 19:00:53 -0600

    added TODO

--- src/app/piratepoker/piratepoker.component.ts
+++ src/app/piratepoker/piratepoker.component.ts
@@ -14,6 +14,11 @@ export class PiratepokerComponent implements OnInit {
   private readonly CARD_WIDTH_PIXELS: string = "60px";
   private readonly CARD_MARGIN: string = "4px";
 
+  // TODO inject cards as JS elements onto the screen
+
+  // GOAL: put 10 random cards on the screen
+  // GOAL: put 10 random cards on the screen, from one deck
+
   constructor() { }
 
   ngOnInit() {
